3ce9a3e968b14806ae3988d3c40b47da6691d568
Allan da Silva <allandasilva.dev@gmail.com>
2022-05-06 11:26:13 -0300

    Cria os testes unitarios do model Warehouse

--- spec/models/warehouse_spec.rb
+++ spec/models/warehouse_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Warehouse, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  describe '#valid?' do
+    context 'presence' do
+      it 'false when name is empty' do
+        # Arrange
+        warehouse = Warehouse.new(name: '', code: 'Rio', address: 'Av. Principal, 200', cep: '26000-000', city: 'Rio', area: 1000, description: 'Galpão do Rio')
+
+        # Act
+        response = warehouse.valid?
+
+        # Assert
+        expect(response).to eq false
+      end
+    end
+
+    context 'unique' do
+      it 'false when code is already in use' do
+        # Arrange
+        first_warehouse = Warehouse.create(name: 'Rio', code: 'Rio', address: 'Av. Principal, 200', cep: '26000-000', city: 'Rio', area: 1000, description: 'Galpão do Rio')
+        second_warehouse = Warehouse.new(name: 'Maceio', code: 'Rio', address: 'Av. Maritima, 200', cep: '52000-000', city: 'Maceio', area: 20000, description: 'Galpão de Maceio')
+
+        # Act
+        response = second_warehouse.valid?
+
+        # Assert
+        expect(response).to eq false
+      end
+    end
+  end
 end
