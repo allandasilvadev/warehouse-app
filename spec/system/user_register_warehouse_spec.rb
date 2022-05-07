12cd25d487e472407ce771517cc186141a910b6a
Allan da Silva <allandasilva.dev@gmail.com>
2022-05-07 09:46:46 -0300

    Adiciona a funcionalidade de editar galpao

--- spec/system/user_register_warehouse_spec.rb
+++ spec/system/user_register_warehouse_spec.rb
@@ -54,5 +54,6 @@ describe 'Usuário cadastra um galpão' do
 
     # Assert
     expect(page).to have_content 'Galpão não cadastrado.'
+    expect(page).to have_content 'Nome não pode ficar em branco'
   end
 end
